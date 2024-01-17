36657f960d5b7bba32a2d67946348cdfe232df27
Do Duc Anh <156392676+Contemporary1408@users.noreply.github.com>
2024-01-17 11:56:28 +0700

    Update InputForm.vb

--- InputForm.vb
+++ InputForm.vb
@@ -1,13 +1,26 @@
-Sub OpenDataEntryForm()
-  Dim nName As Name
-
-  Worksheets("Sheet1").Activate
-
-  Range("C5").CurrentRegion.Name = "database"
-  ActiveSheet.ShowDataForm
-'Luu y phai define name cho bang o vi tri "database"
-
-  For Each nName In ActiveWorkbook.Names
-    If "database" = nName.Name Then nName.Delete
-  Next nName
+Private Sub UserForm_Initialize()
+  'Luu y chon Userform & Initialize
+'Empty all other text box fields
+   txtname.Value = ""
+   txtdept.Value = ""
+ *Tham khao: https://www.tutorialspoint.com/vba/vba_userforms.htm  
+End Sub
+'***************************
+Private Sub btnsubmit_Click()
+   Dim emptyRow As Long
+  
+   'Make Sheet1 active
+   Sheet1.Activate
+  
+   'Determine emptyRow
+   emptyRow = WorksheetFunction.CountA(Range("A:A")) + 1
+  
+   'Transfer information
+   Cells(emptyRow, 1).Value = txtname.Value
+   Cells(emptyRow, 2).Value = txtdept.Value
+  
+End Sub
+'*********************
+Private Sub btncancel_Click()
+   Unload Me
 End Sub
